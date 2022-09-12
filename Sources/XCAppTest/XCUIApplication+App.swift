0be2c9931852152f6ba84e7344be1b72b97d4091
Łukasz Rutkowski <lukus178@gmail.com>
2022-09-12 15:04:37 +0200

    Add assertIsInForeground

--- Sources/XCAppTest/XCUIApplication+App.swift
+++ Sources/XCAppTest/XCUIApplication+App.swift
@@ -11,6 +11,21 @@ extension XCUIApplication {
         _ = wait(for: .runningBackground, timeout: 3) || wait(for: .runningBackgroundSuspended, timeout: 3)
     }
 
+    /// Asserts that the application is currently in foreground.
+    ///
+    /// - Parameters:
+    ///   - message: An optional description of a failure.
+    ///   - file: The file where the failure occurs. The default is the filename of the test case where you call this function.
+    ///   - line: The line number where the failure occurs. The default is the line number where you call this function.
+    public func assertIsInForeground(
+        _ message: @autoclosure () -> String? = nil,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        _ = wait(for: .runningForeground, timeout: 3)
+        assertPredicate("\(#keyPath(XCUIApplication.state)) == \(XCUIApplication.State.runningForeground.rawValue)", message: message() ?? "Application should be in foreground", file: file, line: line)
+    }
+
     /// Asserts that the application is not currently in foreground.
     ///
     /// - Parameters:
